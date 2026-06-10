dabb587e9b8bfa6462df2d5dfef2704d5de328f3
Maxim <kerwprod220717@yandex.ru>
2026-06-11 00:48:50 +0300

    first commit

--- src/js/Character.js
+++ src/js/Character.js
@@ -17,4 +17,23 @@ export default class Character {
         this.attack = attack;
         this.defence = defence;
     }
+
+    levelUp() {
+        if (this.health > 0) {
+            this.level += 1;
+            this.attack *= 1.2;
+            this.defence *= 1.2;
+            this.health = 100;
+        } else {
+            throw new Error('You cannot raise the level with zero health');
+        }
+    }
+
+    damage(points) {
+        this.health -= points * (1 - this.defence / 100);
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
 }
+
--- src/js/__test__/Character.test.js
+++ src/js/__test__/Character.test.js
@@ -24,6 +24,47 @@ test('Invalid character type', () => {
     ).toThrow('Тип должен быть одним из');
 });
 
+test('Inflict damage to character', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.damage(50);
+    expect(testChar).toEqual({
+        name: 'Bowy',
+        type: 'Bowman',
+        health: 62.5,
+        level: 1,
+        attack: 25,
+        defence: 25,
+    });
+});
+
+test('The value of health cannot be negative', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.health = 0;
+    testChar.damage(50);
+    expect(testChar.health).toEqual(0);
+});
+
+test('Go to the next level', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.levelUp();
+    expect(testChar).toEqual({
+        name: 'Bowy',
+        type: 'Bowman',
+        health: 100,
+        level: 2,
+        attack: 30,
+        defence: 30,
+    });
+});
+
+test('Go to the next level with zero health', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.health = 0;
+    expect(() => testChar.levelUp()).toThrow(
+        'You cannot raise the level with zero health',
+    );
+});
+
 test('Bowman', () => {
     const bowman = new Bowman('Bowy');
     expect(bowman).toEqual({
